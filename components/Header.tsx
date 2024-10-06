a0216d4c3d7be1e2d383828ce77cb000d865edd2
ericktek <erickbale360@gmail.com>
2024-10-06 09:42:38 +0300

    add navbar

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,31 +1,90 @@
+import {Navbar, NavbarBrand, NavbarMenuToggle, NavbarMenu, NavbarMenuItem, NavbarContent, NavbarItem, Link, Button} from "@nextui-org/react";
 import { PiBuildingApartment } from "react-icons/pi";
-import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button } from "@nextui-org/react";
-import ToggleMenu from "./ToggleMenu";
 
 const Header = () => {
+  const menuItems = [
+    {
+      id:  1,
+      label: "Home",
+      href: "/",
+    },
+    {
+      id:  2,
+      label: "Profile",
+      href: "/profile",
+    },
+    {
+      id:  3,
+      label: "Dashboard",
+      href: "/dashboard",
+    },
+    {
+      id:  4,
+      label: "Log In",
+      href: "/login",
+    }
+  ];
+
   return (
-    <Navbar maxWidth="2xl" className="max-wfull mx-auto" shouldHideOnScroll>
-      <NavbarBrand >
+    <Navbar maxWidth="2xl" disableAnimation isBordered>
+      <NavbarContent className="sm:hidden" justify="start">
+        <NavbarMenuToggle />
+
+      {/* smaller medium screen */}
+      <NavbarContent className="sm:hidden" justify="start">
+        <NavbarBrand>
         <div className="flex flex-nowrap justify-items-end items-end gap-2">
-          <PiBuildingApartment className="size-8 text-gray-950" />
+           <PiBuildingApartment className="size-7 text-gray-950" />
+          <p className="font-bold text-inherit uppercase hidden sm:flex">Sam Apartment</p>
+        </div>
+        </NavbarBrand>
+      </NavbarContent>
+      </NavbarContent>
+
+
+      {/* larger medium screen */}
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        <NavbarBrand>
+        <div className="flex flex-nowrap justify-items-end items-end gap-2">
+           <PiBuildingApartment className="size-8 text-gray-950" />
           <p className="font-bold text-inherit uppercase">Sam Apartment</p>
         </div>
-      </NavbarBrand>
+        </NavbarBrand>
+        
+      </NavbarContent>
 
-      <NavbarContent justify="end" >
-        <NavbarItem className="hidden lg:flex ">
+      <NavbarContent justify="end">
+        <NavbarItem className="hidden lg:flex">
           <Link href="#">Login</Link>
         </NavbarItem>
-        <NavbarItem  className="hidden lg:flex">
+        <NavbarItem>
           <Button as={Link} color="primary" href="#" variant="flat">
             Sign Up
           </Button>
         </NavbarItem>
-        {/* toggle menu */}
-       <ToggleMenu />
       </NavbarContent>
+
+      <NavbarMenu>
+        
+      <p className="font-bold text-inherit uppercase text-lg">Sam Apartment</p>
+
+        {menuItems.map((id, index) => (
+          <NavbarMenuItem key={`${id}-${index}`}>
+            <Link
+              className="w-full"
+              color={
+                index === 1 ? "warning" : index === menuItems.length - 1 ? "danger" : "foreground"
+              }
+              href="#"
+              size="lg"
+            >
+              {id.label}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
     </Navbar>
   );
-};
+}
 
 export default Header;
