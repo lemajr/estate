123e36232da579030db121ddbfda8d083b783925
ericktek <erickbale360@gmail.com>
2024-10-10 12:52:16 +0300

    fix deploy

--- components/swiper.tsx
+++ components/swiper.tsx
@@ -1,5 +1,4 @@
-'use client'
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Swiper as SwiperCore } from 'swiper'; 
 
@@ -24,139 +23,150 @@ import SkeletonComponent from './skeleton';
 const Slider = () => {
   const [thumbsSwiper, setThumbsSwiper] = useState<SwiperCore | null>(null);
   const [activeIndex, setActiveIndex] = useState(0);
-  const [isLoading, setIsLoading] = useState();
+  const [isLoading, setIsLoading] = useState(true); // Initially set loading to true
+
+  useEffect(() => {
+    // Simulate an image loading process
+    const loadImages = async () => {
+      // Replace with actual loading logic if needed
+      await new Promise(resolve => setTimeout(resolve, 1000)); // Simulate loading delay
+      setIsLoading(false); // Set loading to false after images are loaded
+    };
+
+    loadImages();
+  }, []);
 
   return isLoading ? 
-   (
+    (
       <SkeletonComponent />
-  ): (
-    <>
-      <div className="flex-1 w-full rounded-r-2xl lg:w-1/2 lg:h-[34rem] h-56 sm:h-64 md:h-72 pt-8">
-      {/* Main Slider */}
-      <Swiper
-        spaceBetween={10}
-        navigation={true}
-        loop={true} // Enables looping for the main slider
-        thumbs={{ swiper: thumbsSwiper && !thumbsSwiper.destroyed ? thumbsSwiper : null }}
-        onSlideChange={(swiper) => setActiveIndex(swiper.realIndex)} // Update active index
-        modules={[FreeMode, Navigation, Thumbs]}
-        className="mySwiper2 w-full h-[90%] "
-      >
-        <SwiperSlide>
-          <Image
-            height={400}
-            width={400}
-            alt='Image 1'
-            src={image1}
-            className="w-full rounded-l-full  h-72 lg:h-[34rem] object-cover"
-          />
-        </SwiperSlide>
+    ) : (
+      <>
+        <div className="flex-1 w-full rounded-r-2xl lg:w-1/2 lg:h-[34rem] h-56 sm:h-64 md:h-72 pt-8">
+          {/* Main Slider */}
+          <Swiper
+            spaceBetween={10}
+            navigation={true}
+            loop={true} // Enables looping for the main slider
+            thumbs={{ swiper: thumbsSwiper && !thumbsSwiper.destroyed ? thumbsSwiper : null }}
+            onSlideChange={(swiper) => setActiveIndex(swiper.realIndex)} // Update active index
+            modules={[FreeMode, Navigation, Thumbs]}
+            className="mySwiper2 w-full h-[90%] "
+          >
+            <SwiperSlide>
+              <Image
+                height={400}
+                width={400}
+                alt='Image 1'
+                src={image1}
+                className="w-full rounded-l-full  h-72 lg:h-[34rem] object-cover"
+              />
+            </SwiperSlide>
 
-        <SwiperSlide>
-          <Image
-            height={400}
-            width={400}
-            alt='Image 2'
-            src={image2}
-            className="w-full h-72 rounded-l-full lg:h-[34rem] object-cover"
-          />
-        </SwiperSlide>
+            <SwiperSlide>
+              <Image
+                height={400}
+                width={400}
+                alt='Image 2'
+                src={image2}
+                className="w-full h-72 rounded-l-full lg:h-[34rem] object-cover"
+              />
+            </SwiperSlide>
 
-        <SwiperSlide>
-          <Image
-            height={400}
-            width={400}
-            alt='Image 3'
-            src={image3}
-            className="w-full h-72 rounded-l-full lg:h-[34rem] object-cover"
-          />
-        </SwiperSlide>
+            <SwiperSlide>
+              <Image
+                height={400}
+                width={400}
+                alt='Image 3'
+                src={image3}
+                className="w-full h-72 rounded-l-full lg:h-[34rem] object-cover"
+              />
+            </SwiperSlide>
 
-        <SwiperSlide>
-          <Image
-            height={400}
-            width={400}
-            alt='Image 4'
-            src={image4}
-            className="w-full h-72 rounded-l-full lg:h-[34rem] object-cover"
-          />
-        </SwiperSlide>
+            <SwiperSlide>
+              <Image
+                height={400}
+                width={400}
+                alt='Image 4'
+                src={image4}
+                className="w-full h-72 rounded-l-full lg:h-[34rem] object-cover"
+              />
+            </SwiperSlide>
 
-        <SwiperSlide>
-          <Image
-            height={400}
-            width={400}
-            alt='Image 5'
-            src={image5}
-            className="w-full h-72 rounded-l-full lg:h-[34rem] object-cover"
-          />
-        </SwiperSlide>
-      </Swiper>
+            <SwiperSlide>
+              <Image
+                height={400}
+                width={400}
+                alt='Image 5'
+                src={image5}
+                className="w-full h-72 rounded-l-full lg:h-[34rem] object-cover"
+              />
+            </SwiperSlide>
+          </Swiper>
 
-      {/* Thumbnail Slider */}
-      <Swiper
-        onSwiper={(swiper) => setThumbsSwiper(swiper)}
-        spaceBetween={10}
-        slidesPerView={3}
-        freeMode={true}
-        watchSlidesProgress={true}
-        loop={true} // Enable looping for thumbnails
-        modules={[FreeMode, Navigation, Thumbs]}
-        className="mySwiper w-full ml-48 h-[10%] box-border mt-2  flex justify-end"
-        >
-        <SwiperSlide>
-          <Image
-            height={400}
-            width={400}
-            alt='Thumbnail 1'
-            src={image1}
-            className={`w-full h-10 rounded-l-full object-cover ${activeIndex === 0 ? 'opacity-100' : 'opacity-40'}`}
-          />
-        </SwiperSlide>
+          {/* Thumbnail Slider */}
+          <Swiper
+            onSwiper={(swiper) => setThumbsSwiper(swiper)}
+            spaceBetween={10}
+            slidesPerView={3}
+            freeMode={true}
+            watchSlidesProgress={true}
+            loop={true} // Enable looping for thumbnails
+            modules={[FreeMode, Navigation, Thumbs]}
+            className="mySwiper w-full ml-48 h-[10%] box-border mt-2  flex justify-end"
+          >
+            <SwiperSlide>
+              <Image
+                height={400}
+                width={400}
+                alt='Thumbnail 1'
+                src={image1}
+                className={`w-full h-10 rounded-l-full object-cover ${activeIndex === 0 ? 'opacity-100' : 'opacity-40'}`}
+              />
+            </SwiperSlide>
 
-        <SwiperSlide>
-          <Image
-            height={400}
-            width={400}
-            alt='Thumbnail 2'
-            src={image2}
-            className={`w-full h-10 rounded-l-full object-cover ${activeIndex === 1 ? 'opacity-100' : 'opacity-40'}`}
-          />
-        </SwiperSlide>
+            <SwiperSlide>
+              <Image
+                height={400}
+                width={400}
+                alt='Thumbnail 2'
+                src={image2}
+                className={`w-full h-10 rounded-l-full object-cover ${activeIndex === 1 ? 'opacity-100' : 'opacity-40'}`}
+              />
+            </SwiperSlide>
 
-        <SwiperSlide>
-          <Image
-            height={400}
-            width={400}
-            alt='Thumbnail 3'
-            src={image3}
-            className={`w-full h-10 rounded-l-full object-cover ${activeIndex === 2 ? 'opacity-100' : 'opacity-40'}`}
-          />
-        </SwiperSlide>
+            <SwiperSlide>
+              <Image
+                height={400}
+                width={400}
+                alt='Thumbnail 3'
+                src={image3}
+                className={`w-full h-10 rounded-l-full object-cover ${activeIndex === 2 ? 'opacity-100' : 'opacity-40'}`}
+              />
+            </SwiperSlide>
 
-        <SwiperSlide>
-          <Image
-            height={400}
-            width={400}
-            alt='Thumbnail 4'
-            src={image4}
-            className={`w-full h-10 rounded-l-full object-cover ${activeIndex === 3 ? 'opacity-100' : 'opacity-40'}`}
-          />
-        </SwiperSlide>
+            <SwiperSlide>
+              <Image
+                height={400}
+                width={400}
+                alt='Thumbnail 4'
+                src={image4}
+                className={`w-full h-10 rounded-l-full object-cover ${activeIndex === 3 ? 'opacity-100' : 'opacity-40'}`}
+              />
+            </SwiperSlide>
 
-        <SwiperSlide>
-          <Image
-            height={400}
-            width={400}
-            alt='Thumbnail 5'
-            src={image5}
-            className={`w-full h-10 rounded-l-full object-cover ${activeIndex === 4 ? 'opacity-100' : 'opacity-40'}`}
-          />
-        </SwiperSlide>
-      </Swiper>
-      </div>
-    </>
-  );
+            <SwiperSlide>
+              <Image
+                height={400}
+                width={400}
+                alt='Thumbnail 5'
+                src={image5}
+                className={`w-full h-10 rounded-l-full object-cover ${activeIndex === 4 ? 'opacity-100' : 'opacity-40'}`}
+              />
+            </SwiperSlide>
+          </Swiper>
+        </div>
+      </>
+    );
 };
 
 export default Slider;
